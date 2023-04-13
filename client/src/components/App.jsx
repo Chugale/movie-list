ea1a831a5b06e324672931506017934e5b15710e
Eric Lee <leric7135@gmail.com>
2023-04-12 22:29:13 -0700

    still working on level 3. Also need to figure out why with handlers, pressing enter works, but when they aren't included, it no longer works

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import testData from './TestData.js';
+import MovieList from './MovieList.jsx';
+import MovieObj from './MovieObj.jsx';
+import MovieSubmit from './MovieSubmit.jsx'
+import SearchList from './SearchList.jsx'
+const {useState, useEffect} = React;
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+
+const App = (props) => {
+  //useStates
+//need master list
+  const [masterList, setMasterList] = useState([]);
+  const [revisedList, setRevisedList] = useState(masterList);
+  const [searchString, setSearchString] = useState(''); //search states
+  const [addMovie, setAddMovie] = useState('');
+
+  // const [movieState, setMovieState] = useState('');
+  let timeout = null;
+  useEffect(() => {
+    timeout = setTimeout(() => {
+      if (searchString.length > 0) {
+        setRevisedList(masterList.filter(
+          movie => movie.title.toLowerCase().includes(searchString.toLowerCase())
+          ));
+      } else if (searchString.length === 0) {
+        setRevisedList(masterList);
+      }
+    }, 300)
+
+    return () => clearTimeout(timeout);
+  }, [searchString]);
+
+  useEffect(() => {
+    setRevisedList(masterList);
+  }, [masterList])
+
+  //DEPRECATED HANDLERS
+  //handlers
+  // const searchHandler = (event) => {//Handle
+  //   event.preventDefault();
+  //   const filterStringPresence = (movie) => {
+  //     // console.log(searchString, movie)
+  //     if(movie.title.indexOf(searchString) !== -1) {
+  //       return true
+  //     }
+  //   }
+  //   if(searchString.length === 0) {
+  //     return setRevisedList(masterList)
+  //   } else {
+  //     let listSearched = revisedList.filter(filterStringPresence);
+  //     setRevisedList(listSearched)
+  //   }
+  // }
+  // const moveStateHandler = (event) => {
+  //   event.preventDefault();
+
+  // }
+
+  //rest of APP
+  return (
+    <div>
+      <header>
+        <h1>
+          Movie List
+        </h1>
+      </header>
+      <div className="list-controller">
+        {/* <button onClick={() => {setRevisedList(masterList)}}>Return Home</button> */}
+
+        <MovieSubmit addMovie={addMovie} setAddMovie={setAddMovie} masterList={masterList} setMasterList={setMasterList} setRevisedList={setRevisedList}/>
+
+        <SearchList setSearchString={setSearchString}/>
+        {/* //add watched and to Watch */}
+      </div>
+        <MovieList masterList={masterList} setMasterList={setMasterList} revisedList={revisedList} />
+    </div>
+  )
+};
+
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+// import MovieList from './components/MovieList.jsx';
+// import MovieTitle from './components/MovieTitle.jsx';
+
+var movies = [
+  { title: 'Mean Girls' },
+  { title: 'Hackers' },
+  { title: 'The Grey' },
+  { title: 'Sunshine' },
+  { title: 'Ex Machina' },
+];
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.createRoot('root').render(<app/>)
